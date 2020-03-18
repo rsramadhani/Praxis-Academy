ad19641b93891cea7e20db416ec602745efa2184
rsramadhani <rosiramadhani16@gmail.com>
2020-03-18 12:46:26 +0700

    update

---
+++ Novice/02-04/latihan/animator2/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(LogoApp());
+
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: animat ion.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
+    );
+  }
+}
+
+class LogoApp extends StatefulWidget {
+  _LogoAppState createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
---
+++ Novice/02-05/latihan/lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Collapsing List Demo')),
+        body: CollapsingList(),
+      ),
+    );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate({
+    @required this.minHeight,
+    @required this.maxHeight,
+    @required this.child,
+  });
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+  @override
+  double get minExtent => minHeight;
+  @override
+  double get maxExtent => math.max(maxHeight, minHeight);
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
+  }
+}
+
+class CollapsingList extends StatelessWidget {
+  SliverPersistentHeader makeHeader(String headerText) {
+    return SliverPersistentHeader(
+      pinned: true,
+      delegate: _SliverAppBarDelegate(
+        minHeight: 60.0,
+        maxHeight: 200.0,
+        child: Container(
+            color: Colors.white, child: Center(child: Text(headerText))),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        makeHeader('Header Section 1'),
+        SliverGrid.count(
+          crossAxisCount: 3,
+          children: [
+            Container(color: Colors.pink[50], height: 150.0),
+            Container(color: Colors.brown[100], height: 150.0),
+            Container(color: Colors.teal[100], height: 150.0),
+            Container(color: Colors.lightGreen[100], height: 150.0),
+            Container(color: Colors.lightBlue[100], height: 150.0),
+            Container(color: Colors.lime[200], height: 150.0),
+            Container(color: Colors.cyan[200], height: 150.0),
+            Container(color: Colors.black, height: 150.0),
+            Container(color: Colors.red[200], height: 150.0),
+          ],
+        ),
+        makeHeader('Header Section 2'),
+        SliverFixedExtentList(
+          itemExtent: 150.0,
+          delegate: SliverChildListDelegate(
+            [
+              Container(color: Colors.black),
+              Container(color: Colors.red[100]),
+              Container(color: Colors.red[50]),
+              Container(color: Colors.grey),
+              Container(color: Colors.redAccent),
+            ],
+          ),
+        ),
+        makeHeader('Header Section 3'),
+        SliverGrid(
+          gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200.0,
+            mainAxisSpacing: 10.0,
+            crossAxisSpacing: 10.0,
+            childAspectRatio: 4.0,
+          ),
+          delegate: new SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return new Container(
+                alignment: Alignment.center,
+                color: Colors.grey[100 * (index % 9)],
+                child: new Text('grid item $index'),
+              );
+            },
+            childCount: 20,
+          ),
+        ),
+        makeHeader('Header Section 4'),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              Container(color: Colors.indigo, height: 150.0),
+              Container(color: Colors.black, height: 150.0),
+              Container(color: Colors.red[100], height: 150.0),
+              Container(color: Colors.grey, height: 150.0),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
