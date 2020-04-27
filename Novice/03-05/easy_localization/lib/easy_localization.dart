b2bba10be78aca118b81652d7b4e36b14d6ef4e0
rsramadhani <rosiramadhani16@gmail.com>
2020-04-27 07:20:23 +0700

    update

--- Novice/03-05/easy_localization/lib/easy_localization.dart
+++
@@ -1,8 +0,0 @@
-library easy_localization;
-
-export 'package:easy_localization/src/easy_localization_app.dart';
-export 'package:easy_localization/src/asset_loader.dart';
-export 'package:easy_localization/src/public.dart';
-export 'package:easy_localization/src/public_ext.dart';
-export 'package:easy_localization/src/bloc/easy_localization_bloc.dart';
-export 'package:intl/intl.dart';
